8d9b7c1dc99b326f044c343aca70e4f092326def
s11199835 <s11199835@LAU048NWLAB05.student.usp.ac.fj>
2023-09-27 14:37:15 +1200

    shivi - 2

---
+++ src/main/java/com/mycompany/for341/For341.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.for341;
+
+/**
+ *
+ * @author s11199835
+ */
+public class For341 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("done by shivi");
+    }
+}
